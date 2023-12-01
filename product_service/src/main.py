830a2ea1c975fbe78f8faa98633e7d5110eb6950
Aliaksei Piantsiak <avpentyak@gmail.com>
2023-12-01 13:36:38 +0100

    feat: Improve create_product transaction-based functionality

--- product_service/src/main.py
+++ product_service/src/main.py
@@ -1,7 +1,10 @@
+import json
+import logging
 from decimal import Decimal
 from uuid import uuid4
 
 import boto3
+from boto3.dynamodb.types import TypeSerializer
 from fastapi import (
     FastAPI,
     HTTPException,
@@ -20,7 +23,14 @@ from pydantic import BaseModel, Field
 
 app = FastAPI(title="ProductApi", version="0.1")
 
-handler = Mangum(app)
+
+# handler = Mangum(app)
+
+def handler(event, context):
+    logging.info(msg=json.dumps(event, indent=4))
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context)
+    return response
 
 
 def get_dynamo_client() -> DynamoDBClient:
@@ -53,7 +63,7 @@ def validation_exception_handler(
     )
 
 
-@app.get("/products", response_model=list[ProductsResponse])
+@app.get(path="/products", response_model=list[ProductsResponse])
 async def products_list(
         db_client: DynamoDBClient = Depends(get_dynamo_client)
 ):
@@ -72,7 +82,7 @@ async def products_list(
     return products
 
 
-@app.get("/products/{product_id}", response_model=ProductsResponse)
+@app.get(path="/products/{product_id}", response_model=ProductsResponse)
 async def product_by_id(
         product_id: str = Path(example="207ac7a46e434349b55e0daef544aeb6"),
         db_client: DynamoDBClient = Depends(get_dynamo_client)
@@ -89,56 +99,51 @@ async def product_by_id(
     return product
 
 
-@app.post(
-    path="/products",
-    response_model=ProductsResponse,
-)
+@app.post(path="/products", response_model=ProductsResponse)
 async def create_product(
         response: Response,
         product_request: ProductsRequest,
+        db_serializer: TypeSerializer = Depends(TypeSerializer),
 ):
     try:
         new_product_id = f"{uuid4().hex}"
 
-        new_product_response = {
+        new_product = {
             "id": new_product_id,
             "price": product_request.price,
             "title": product_request.title,
             "description": product_request.description,
         }
 
+        new_stock = {
+            "product_id": new_product_id,
+            "count": product_request.count,
+        }
+
+        queue = [
+            ("Put", table, item)
+            for table, item in [("Products", new_product), ("Stocks", new_stock)]
+        ]
+
         boto3.client("dynamodb").transact_write_items(
             TransactItems=[
                 {
-                    'Put': {
-                        'TableName': 'Products',
+                    method: {
+                        'TableName': table,
                         'Item': {
-                            'id': {'S': f"{new_product_id}"},
-                            'price': {'N': f"{product_request.price}"},
-                            'title': {'S': product_request.title},
-                            'description': {'S': product_request.description},
+                            key: db_serializer.serialize(value)
+                            for key, value in item.items()
                         },
                         'ConditionExpression': 'attribute_not_exists(id)',
                         "ReturnValuesOnConditionCheckFailure": "ALL_OLD",
                     }
-                },
-                {
-                    'Put': {
-                        'TableName': 'Stocks',
-                        'Item': {
-                            'product_id': {'S': f"{new_product_id}"},
-                            'count': {'N': f"{product_request.count}"},
-                        },
-                        'ConditionExpression': 'attribute_not_exists(product_id)',
-                        "ReturnValuesOnConditionCheckFailure": "ALL_OLD",
-                    }
-                },
+                } for method, table, item in queue
             ]
         )
     except Exception as err:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         response.body = {"details": err}
     else:
-        new_product_response["count"] = product_request.count
+        new_product["count"] = product_request.count
         response.status_code = status.HTTP_201_CREATED
-        return new_product_response
+        return new_product
