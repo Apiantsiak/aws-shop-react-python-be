7cdcb8479d673848664ec66100162479b5a66ec6
Aliaksei Piantsiak <avpentyak@gmail.com>
2023-12-17 18:08:06 +0100

    feat: Implement auth service - Create basic token authorizer lambda

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import aws_cdk as cdk
 
 from import_service.import_s3event_lambda_stack import ImportServiceStack
 from product_service.fastapi_dynamo_stack import FastApiProductsStack
+from authorization_service.authorization_lamda_stack import AuthorizationServiceStack
 
 
 app = cdk.App()
 
 FastApiProductsStack(app, construct_id="FastApiProductsServiceStack")
 ImportServiceStack(app, construct_id="ImportServiceStack")
+AuthorizationServiceStack(app, construct_id="AuthorizationServiceStack")
 
 app.synth()
